a130bf137a85fc63ce6d9169ce263d92aba99ac9
karthikKulkarni <karthik.k8687@gmail.com>
2020-10-23 22:56:33 +0530

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=northeurope
-export SIRMIONE_ALIAS=zbpsirmione
-export LIMONE_ALIAS=zbplimone
-export SCORPIO_ALIAS=zbpscorpio
-export VIRGO_ALIAS=zbpvirgo
-export LIBRA_ALIAS=zbplibra
-export ZODIAC_ALIAS=zbpzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=kk1812sirmione
+export LIMONE_ALIAS=kk1812limone
+export SCORPIO_ALIAS=kk1812scorpio
+export VIRGO_ALIAS=kk1812virgo
+export LIBRA_ALIAS=kk1812libra
+export ZODIAC_ALIAS=kk1812zodiac
+export DB_ADMIN_USER=Karthik
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
 export OUTPUT=json
 
 # Whatever you set zodiac instance to will be tagged onto your azure resources, and enable you to access all the different components as a cohesive set
-export ZODIAC_INSTANCE=halloween
+export ZODIAC_INSTANCE=kk1812
 
 echo "<h2>Environment Variables</h2>" >> deployment-log.html
 echo "<p>ZODIAC_INSTANCE:$ZODIAC_INSTANCE</p>" >> deployment-log.html
